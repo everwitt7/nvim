cdbd8c97310adc7559dd02306ac6056da1c42819
Everett Witt <everwitt7@gmail.com>
2023-06-04 18:14:55 -0500

    adding toggleterm for a terminal to run things
    this is like vscode's terminal which can probably be useful to recompile
    something over and over to make sure that it is working - there are
    probably commands to just run tests and compile code without having to
    enter a terminal. Also, for autocompletions I cycle using <Tab> and can
    select using Ctrl+y

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,67 +1,73 @@
 return {
-    -- Which-key Extension
-    {
-      "folke/which-key.nvim",
-      lazy = true,
-    },
-    -- Colorscheme
-    {
-      "folke/tokyonight.nvim",
-      lazy = false,
-      priority = 1000,
-      opts = {},
-    },
-    { "ellisonleao/gruvbox.nvim", priority = 1000 },
+  -- Which-key Extension
+  {
+    "folke/which-key.nvim",
+    lazy = true,
+  },
+  -- Colorscheme
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  { "ellisonleao/gruvbox.nvim", priority = 1000 },
 
-    -- Lualine (THE NEW EXTENSION ADDED IN CONFIGURATION)
-    {
-      'nvim-lualine/lualine.nvim',
-      dependencies = { 'nvim-tree/nvim-web-devicons', opt = true}
+  -- Lualine (THE NEW EXTENSION ADDED IN CONFIGURATION)
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons', opt = true }
+  },
+  -- Nvimtree (File Explorer)
+  {
+    'nvim-tree/nvim-tree.lua',
+    lazy = true,
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
     },
-    -- Nvimtree (File Explorer)
-    {
-        'nvim-tree/nvim-tree.lua',
-        lazy = true,
-        dependencies = {
-            'nvim-tree/nvim-web-devicons',
-        },
-    },
-    -- Telescope (fuzzy file finding)
-    {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
--- or                              , branch = '0.1.1',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    -- Alpha (Dashboard)
-    {
-        "goolord/alpha-nvim",
-        lazy = true,
-    },
-    -- Treesitter
-    {
-        "nvim-treesitter/nvim-treesitter",
-    },
-    -- Language Support
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        dependencies = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {'williamboman/mason.nvim'},           -- Optional
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+  },
+  -- Telescope (fuzzy file finding)
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.1',
+    -- or                              , branch = '0.1.1',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  -- Alpha (Dashboard)
+  {
+    "goolord/alpha-nvim",
+    lazy = true,
+  },
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+  },
+  -- Language Support
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },             -- Required
+      { 'williamboman/mason.nvim' },           -- Optional
+      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},         -- Required
-            {'hrsh7th/cmp-nvim-lsp'},     -- Required
-            {'hrsh7th/cmp-buffer'},       -- Optional
-            {'hrsh7th/cmp-path'},         -- Optional
-            {'saadparwaiz1/cmp_luasnip'}, -- Optional
-            {'hrsh7th/cmp-nvim-lua'},     -- Optional
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },         -- Required
+      { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+      { 'hrsh7th/cmp-buffer' },       -- Optional
+      { 'hrsh7th/cmp-path' },         -- Optional
+      { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+      { 'hrsh7th/cmp-nvim-lua' },     -- Optional
 
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},             -- Required
-            {'rafamadriz/friendly-snippets'}, -- Optional
-        }
-    },
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },             -- Required
+      { 'rafamadriz/friendly-snippets' }, -- Optional
+    }
+  },
+  -- Added this plugin.
+  {
+    'akinsho/toggleterm.nvim',
+     onfig = true
+  },
 }
