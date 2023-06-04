3ab552fbc3640230850549eabd4bf517b82833f0
Everett Witt <everwitt7@gmail.com>
2023-06-04 18:28:10 -0500

    added surround AND call setup in `plugins.lua`
    nvim surround is supposed to make it easy to take some words and put
    quotes or parenths or other pairs around the text, so this is similar in
    a way to the autopairs, but basically you forgot to type the quote
    initially or you are changing something, etc... I also learned that you
    can just put the `setup` function call directly in plugins, which means
    that you do not need to create a configuration file for every single
    plugin that you want

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -65,12 +65,23 @@ return {
       { 'rafamadriz/friendly-snippets' }, -- Optional
     }
   },
-  -- Added this plugin.
+  -- ToggleTerm
   {
     'akinsho/toggleterm.nvim',
     config = true
   },
+  -- Auto Pairs
   {
     "windwp/nvim-autopairs"
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
   },
 }
