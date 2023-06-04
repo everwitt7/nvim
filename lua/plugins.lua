f371ee00a6213f2900e1b4993fb33086a7165115
Everett Witt <everwitt7@gmail.com>
2023-06-04 18:43:48 -0500

    adding indentlines in plugins
    this adds the vertical lines to make the tabs easier to see. There is
    also an option to add something to show where enters (carriage returns)
    are in the code too. I think that I need to go back to nvimtree and
    figure out why the icons are not showing. That and some of the options
    that I set to indent text do not seem to be working, but I think
    everything else is

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -91,4 +91,16 @@ return {
       "nvim-lua/plenary.nvim",
     },
   },
+  -- IndentLine
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      require("indent_blankline").setup({
+        -- for example, context is off by default, use this to turn it on
+        show_current_context = true,
+        show_current_context_start = true,
+      })
+    end
+  },
+
 }
